21284b4c81bbfaaec9e07594906eeccde7734dbf
setLylToxlx <781921852@qq.com>
2016-08-31 10:52:27 +0800

    更新链接

--- javascripts/weixin.js
+++ javascripts/weixin.js
@@ -2,4 +2,15 @@ define(function(require,exports,module){
 	// alert(1);
 	// var $ = require('jquery.min.js');
 	console.log($('.container').html());
+	wx.onMwnuShareTimeline:function(){
+		title:'我的标题',
+		link:"https://setlyltoxlx.github.io/818.html",
+		imgUrl:'https://setlyltoxlx.github.io/setlyltoxlx.github.io/images/apple.png',
+		success:function(){
+			alert('分享成功！');
+		},
+		cancel:function(){
+			alert('取消分享！');
+		}
+	}
 })
