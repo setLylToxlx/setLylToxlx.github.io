21258424ce15a3e5f1340153470a5aa8542bee2e
setLylToxlx <781921852@qq.com>
2016-08-31 14:44:58 +0800

    更新

--- javascripts/weixin.js
+++ javascripts/weixin.js
@@ -5,19 +5,20 @@ define(function(require,exports,module){
 	
 
 	wx.ready(function(){
-		$('#shareWin').click(function(){
+		document.querySelector('#onMenuShareTimeline').onclick = function () {
 			wx.onMenuShareTimeline({
-				title:'我的标题',
-				link:"https://setlyltoxlx.github.io/818.html",
-				imgUrl:'https://setlyltoxlx.github.io/setlyltoxlx.github.io/images/apple.png',
-				success:function(){
+				title: '我的标题',
+				link: "https://setlyltoxlx.github.io/818.html",
+				// desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
+				imgUrl: 'https://setlyltoxlx.github.io/setlyltoxlx.github.io/images/apple.png',
+				success: function(){
 					alert('分享成功！');
 				},
-				cancel:function(){
+				cancel: function(){
 					alert('取消分享！');
 				}
 			})
-		})
+		}
 		
     // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
 	});
