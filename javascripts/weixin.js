07e10d57511ea02313f4dc810877482362267ae9
setLylToxlx <781921852@qq.com>
2016-08-31 15:20:04 +0800

    上传照片

--- javascripts/weixin.js
+++ javascripts/weixin.js
@@ -17,8 +17,16 @@ define(function(require,exports,module){
 				cancel: function(){
 					alert('取消分享！');
 				}
-			})
-		})
+			});
+			$('#uploadImg').on('click',function(){
+				count:1,
+				sizeType: ['original','compressed'],
+				sourceType:['albun','camera'],
+				success:function(res){
+					var localIds = res.localIds;
+				}
+			});
+		});
 		
     // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
 	});
